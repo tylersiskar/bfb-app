6b54f06f2e191e7755b61b5247131ae5b14835fa
tylersiskar <36086162+tylersiskar@users.noreply.github.com>
2024-11-21 21:47:54 -0500

    helmet consle error

--- src/pages/mock/mocks-new.jsx
+++ src/pages/mock/mocks-new.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
-import { Helmet } from "react-helmet";
 import { useSelector, useDispatch } from "react-redux";
 import {
   selectDraftOrder,
-  useGetNflStateQuery,
   useGetRostersQuery,
   useGetTradedPicksQuery,
   useGetUsersQuery,
@@ -18,7 +16,6 @@ import {
   mdiChevronLeft,
   mdiCloseBoxOutline,
   mdiListBoxOutline,
-  mdiLoading,
   mdiTrashCanOutline,
 } from "@mdi/js";
 import { Button, IconButton } from "../../components/buttons";
@@ -36,8 +33,6 @@ import {
   selectKeepers,
   useGetMockQuery,
   useGetMocksQuery,
-  useGetPlayersQuery,
-  useLazyGetPlayerValueQuery,
   usePostMockMutation,
   useGetPlayersAllQuery,
 } from "../../api/bfbApi";
@@ -51,8 +46,8 @@ import {
   selectLeagueId,
 } from "../../api/leagueSlice";
 import useActiveRoster from "./useActiveRoster";
-import Icon from "@mdi/react";
 import html2canvas from "html2canvas";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 let URL = `https://s3.amazonaws.com/badfranchisebuilders.com/thumbnails/{IMAGE}`;
 
@@ -215,104 +210,111 @@ const MockNew = () => {
     }
   };
   return (
-    <>
-      <Helmet>
-        <meta
-          property="og:image"
-          content={
-            currentMock && URL.replace("{IMAGE}", currentMock[0].thumbnail)
-          }
-        />
-        <meta property="og:url" content={window.location.href} />
-      </Helmet>
-      <Content dark isLoading={isLoading}>
-        <div className="home-body">
-          <div className="d-flex flex-column">
-            <div className="flex justify-between mb-1">
-              <Link className="flex align-center justify-start p-2" to="/mocks">
-                <IconButton className="p-0" icon={mdiChevronLeft} />
-                <p className="light"> Back</p>
-              </Link>
-            </div>
-            <div className="flex justify-between" style={{ padding: "0 16px" }}>
-              <h2> {`${currentMock ? currentMock[0].name : name}'s Mock`}</h2>
-              {draftedPlayers.length > 0 && (
-                <div>
-                  <Button
-                    className={`${id ? "d-none" : ""} bg-${
-                      name ? "lime" : "gray"
-                    } button-sm flex align-center p-1`}
-                    style={{
-                      borderColor: name ? "#54d846" : "rgb(206, 206, 206)",
-                    }}
-                    onClick={() => name && handleSubmit(draftedPlayers)}
-                    dispatch={!name}
-                  >
-                    <p className="sm dark bold">SUBMIT MOCK</p>
-                  </Button>
-                </div>
-              )}
-            </div>
-            <Draftboard
-              standings={standings}
-              rosters={data}
-              onSelectPick={(pick, roundIdx, pickIdx) => {
-                setRoundIdx(roundIdx);
-                setPickIdx(pickIdx);
-                setExpandList(true);
-              }}
-              selectedPick={activeSlot}
-              draftedPlayers={draftedPlayers}
-              draftOrderWithTrades={draftOrderWithTrades}
-            />
-            <div
-              style={{
-                borderTop: "1px solid white",
-                padding: "8px 16px",
-                marginTop: 8,
-                width: "100%",
-                height: "40svh",
-                overflowY: "auto",
-                boxSizing: "border-box",
-              }}
-            >
-              <h5>Trades Made (Coming soon...)</h5>
-            </div>
-            <RosterPanel
-              isExpanded={openPanel}
-              playerListExpanded={expandList}
-              isVisible={!!Object.keys(activeSlot).length}
-              activeRoster={activeRoster}
-            />
-            <div
-              className={`bottom-drawer ${id ? "d-none" : ""} ${
-                expandList
-                  ? openPanel
-                    ? "half-expanded"
-                    : "expanded"
-                  : "collapsed"
-              }`}
-            >
-              <div className="flex w-100 justify-between">
-                <div>
-                  {!!Object.keys(activeSlot).length ? (
-                    <div className="flex flex-column">
-                      <h6 className="pb-1">Active Draft Selection</h6>
-                      <div className="flex align-end">
-                        <p className="light pb-1 pr-1 bold">
-                          {activeSlot.team}{" "}
-                        </p>
-                        <p className="light sm pb-1">
-                          Pick {activeSlot.round}.{activeSlot.pick}
-                        </p>
+    <HelmetProvider>
+      <>
+        <Helmet>
+          <meta
+            property="og:image"
+            content={
+              currentMock && URL.replace("{IMAGE}", currentMock[0].thumbnail)
+            }
+          />
+          <meta property="og:url" content={window.location.href} />
+        </Helmet>
+        <Content dark isLoading={isLoading}>
+          <div className="home-body">
+            <div className="d-flex flex-column">
+              <div className="flex justify-between mb-1">
+                <Link
+                  className="flex align-center justify-start p-2"
+                  to="/mocks"
+                >
+                  <IconButton className="p-0" icon={mdiChevronLeft} />
+                  <p className="light"> Back</p>
+                </Link>
+              </div>
+              <div
+                className="flex justify-between"
+                style={{ padding: "0 16px" }}
+              >
+                <h2> {`${currentMock ? currentMock[0].name : name}'s Mock`}</h2>
+                {draftedPlayers.length > 0 && (
+                  <div>
+                    <Button
+                      className={`${id ? "d-none" : ""} bg-${
+                        name ? "lime" : "gray"
+                      } button-sm flex align-center p-1`}
+                      style={{
+                        borderColor: name ? "#54d846" : "rgb(206, 206, 206)",
+                      }}
+                      onClick={() => name && handleSubmit(draftedPlayers)}
+                      dispatch={!name}
+                    >
+                      <p className="sm dark bold">SUBMIT MOCK</p>
+                    </Button>
+                  </div>
+                )}
+              </div>
+              <Draftboard
+                standings={standings}
+                rosters={data}
+                onSelectPick={(pick, roundIdx, pickIdx) => {
+                  setRoundIdx(roundIdx);
+                  setPickIdx(pickIdx);
+                  setExpandList(true);
+                }}
+                selectedPick={activeSlot}
+                draftedPlayers={draftedPlayers}
+                draftOrderWithTrades={draftOrderWithTrades}
+              />
+              <div
+                style={{
+                  borderTop: "1px solid white",
+                  padding: "8px 16px",
+                  marginTop: 8,
+                  width: "100%",
+                  height: "40svh",
+                  overflowY: "auto",
+                  boxSizing: "border-box",
+                }}
+              >
+                <h5>Trades Made (Coming soon...)</h5>
+              </div>
+              <RosterPanel
+                isExpanded={openPanel}
+                playerListExpanded={expandList}
+                isVisible={!!Object.keys(activeSlot).length}
+                activeRoster={activeRoster}
+              />
+              <div
+                className={`bottom-drawer ${id ? "d-none" : ""} ${
+                  expandList
+                    ? openPanel
+                      ? "half-expanded"
+                      : "expanded"
+                    : "collapsed"
+                }`}
+              >
+                <div className="flex w-100 justify-between">
+                  <div>
+                    {!!Object.keys(activeSlot).length ? (
+                      <div className="flex flex-column">
+                        <h6 className="pb-1">Active Draft Selection</h6>
+                        <div className="flex align-end">
+                          <p className="light pb-1 pr-1 bold">
+                            {activeSlot.team}{" "}
+                          </p>
+                          <p className="light sm pb-1">
+                            Pick {activeSlot.round}.{activeSlot.pick}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  ) : (
-                    <h6 className="">Select Draft Position</h6>
-                  )}
-                </div>
-                <div className="flex align-center">
-                  {/* {playerValueIsLoading && (
+                    ) : (
+                      <h6 className="">Select Draft Position</h6>
+                    )}
+                  </div>
+                  <div className="flex align-center">
+                    {/* {playerValueIsLoading && (
                     <Icon
                       path={mdiLoading}
                       title={"loading"}
@@ -321,104 +323,107 @@ const MockNew = () => {
                       className="loading-icon"
                     />
                   )} */}
-                  {!!find(draftedPlayers, {
-                    round: activeSlot.round,
-                    pick: activeSlot.pick,
-                  }) && (
-                    <IconButton
-                      icon={mdiTrashCanOutline}
-                      title="Remove Selection"
-                      iconColor="#ff3f5d"
-                      onClick={() => {
-                        dispatch(
-                          updateDraftedPlayers(
-                            draftedPlayers.filter(
-                              (player) =>
-                                !(
-                                  player.round === activeSlot.round &&
-                                  player.pick === activeSlot.pick
-                                )
+                    {!!find(draftedPlayers, {
+                      round: activeSlot.round,
+                      pick: activeSlot.pick,
+                    }) && (
+                      <IconButton
+                        icon={mdiTrashCanOutline}
+                        title="Remove Selection"
+                        iconColor="#ff3f5d"
+                        onClick={() => {
+                          dispatch(
+                            updateDraftedPlayers(
+                              draftedPlayers.filter(
+                                (player) =>
+                                  !(
+                                    player.round === activeSlot.round &&
+                                    player.pick === activeSlot.pick
+                                  )
+                              )
                             )
-                          )
-                        );
-                        // updatePlayerValueList(
-                        //   draftedPlayers.filter(
-                        //     (player) =>
-                        //       !(
-                        //         player.round === activeSlot.round &&
-                        //         player.pick === activeSlot.pick
-                        //       )
-                        //   )
-                        // );
-                      }}
-                    />
-                  )}
-                  {Object.keys(activeSlot).length > 0 && (
+                          );
+                          // updatePlayerValueList(
+                          //   draftedPlayers.filter(
+                          //     (player) =>
+                          //       !(
+                          //         player.round === activeSlot.round &&
+                          //         player.pick === activeSlot.pick
+                          //       )
+                          //   )
+                          // );
+                        }}
+                      />
+                    )}
+                    {Object.keys(activeSlot).length > 0 && (
+                      <IconButton
+                        icon={
+                          openPanel ? mdiCloseBoxOutline : mdiListBoxOutline
+                        }
+                        onClick={() => setOpenPanel(!openPanel)}
+                      />
+                    )}
                     <IconButton
-                      icon={openPanel ? mdiCloseBoxOutline : mdiListBoxOutline}
-                      onClick={() => setOpenPanel(!openPanel)}
+                      icon={expandList ? mdiArrowDownThin : mdiArrowUpThin}
+                      onClick={() => setExpandList(!expandList)}
                     />
-                  )}
-                  <IconButton
-                    icon={expandList ? mdiArrowDownThin : mdiArrowUpThin}
-                    onClick={() => setExpandList(!expandList)}
-                  />
+                  </div>
                 </div>
-              </div>
-              <div className="flex my-2">
-                <Button
-                  className="button-sm inverted mr-2"
-                  id="QB"
-                  onClick={_setPosition}
-                  active={position === "QB"}
-                >
-                  QB
-                </Button>
-                <Button
-                  className="button-sm inverted mr-2"
-                  id="RB"
-                  onClick={_setPosition}
-                  active={position === "RB"}
-                >
-                  RB
-                </Button>
-                <Button
-                  className="button-sm inverted mr-2"
-                  id="WR"
-                  onClick={_setPosition}
-                  active={position === "WR"}
-                >
-                  WR
-                </Button>
-                <Button
-                  className="button-sm inverted mr-2"
-                  id="TE"
-                  onClick={_setPosition}
-                  active={position === "TE"}
-                >
-                  TE
-                </Button>
-                {/* <Button
+                <div className="flex my-2">
+                  <Button
+                    className="button-sm inverted mr-2"
+                    id="QB"
+                    onClick={_setPosition}
+                    active={position === "QB"}
+                  >
+                    QB
+                  </Button>
+                  <Button
+                    className="button-sm inverted mr-2"
+                    id="RB"
+                    onClick={_setPosition}
+                    active={position === "RB"}
+                  >
+                    RB
+                  </Button>
+                  <Button
+                    className="button-sm inverted mr-2"
+                    id="WR"
+                    onClick={_setPosition}
+                    active={position === "WR"}
+                  >
+                    WR
+                  </Button>
+                  <Button
+                    className="button-sm inverted mr-2"
+                    id="TE"
+                    onClick={_setPosition}
+                    active={position === "TE"}
+                  >
+                    TE
+                  </Button>
+                  {/* <Button
                 className="button-sm inverted"
                 onClick={() => setRookies(!rookies ? "true" : "")}
                 active={!!rookies}
               >
                 Rookies
               </Button> */}
+                </div>
+                <PlayerList
+                  onDraft={_onDraft}
+                  scrollHeight={`calc(${
+                    expandList ? (openPanel ? "40svh" : "55svh") : "170px"
+                  } - 136px)`}
+                  playerList={_getPlayerList()}
+                  // playerValueIsFetching={playerValueIsLoading}
+                />
               </div>
-              <PlayerList
-                onDraft={_onDraft}
-                scrollHeight={`calc(${
-                  expandList ? (openPanel ? "40svh" : "55svh") : "170px"
-                } - 136px)`}
-                playerList={_getPlayerList()}
-                // playerValueIsFetching={playerValueIsLoading}
-              />
             </div>
           </div>
-        </div>
-      </Content>
-    </>
+        </Content>
+      </>
+    </HelmetProvider>
   );
 };
 
